7c95b48d54a35d535d241bfc231c8736688b9a02
SergiuAngelo <58261541+SergiuAngelo@users.noreply.github.com>
2026-04-03 16:27:15 +0300

    Update DriverFactory.java

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -3,6 +3,7 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -10,8 +11,19 @@ public class DriverFactory {
 
     public static WebDriver getDriver() {
         if (driver == null) {
+
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+
+            ChromeOptions options = new ChromeOptions();
+
+            // 🔥 CRITICAL pentru GitHub Actions
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(options);
         }
         return driver;
     }
